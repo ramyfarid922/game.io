adc7b40141f9b8d2329c8c84937e2ce40f9bb5b3
Ramy <ramyfarid922@gmail.com>
2021-11-08 06:56:38 +0200

    refactoring winning events into a util function

--- server/index.js
+++ server/index.js
@@ -20,12 +20,16 @@ const checkWin = (socket, win) => {
   }
 };
 
-const connectionHandler = (socket) => {
-  if (game.status === "RUNNING") {
+const checkFull = (socket, status) => {
+  if (status === "RUNNING") {
     // If game has two players and its state is set to "RUNNING"
     // Reject the 3rd player socket connection
     return socket.emit("serverFull", "Connection rejected! game is full");
   }
+};
+
+const connectionHandler = (socket) => {
+  checkFull(socket, game.status);
 
   socket.emit("serverWelcome");
 
@@ -48,21 +52,12 @@ const connectionHandler = (socket) => {
 
   socket.on("inceptNumber", (num) => {
     game.incept(socket.id, num);
-
-    if (game.winner) {
-      // Update the player who made the move that he won
-      socket.emit("youWin");
-      // Update the opponent that they lost
-      socket.broadcast.emit("youLose");
-    } else {
-      socket.broadcast.emit("number");
-    }
+    checkWin(socket, game.winner);
   });
 
   socket.on("sendMove", (move) => {
     game.move(socket.id, move);
     game.log(socket.id, move);
-
     checkWin(socket, game.winner);
   });
 
